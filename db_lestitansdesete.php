ab095bb8fa64ac894c267d810a68ef7e80a9d032
Esther Paqui <paqui.esther@etu.iut-tlse3.fr>
2024-11-26 12:32:05 +0100

    Refactor homepage layout and styles; replace header with navigation component and update CSS files

--- db_lestitansdesete.php
+++ db_lestitansdesete.php
@@ -1,14 +1,15 @@
-<? php
+<?php
 
 // a modifier plus tard pour le serveur en ligne
 $servername = "localhost"; 
 $username = "root";
 $password = "";
 $dbname = "db_lestitansdesete";
+$socket = "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock"; // Chemin du socket MySQL
 
 // créer une connexion
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, null, $socket);
 
 // vérifier la connexion
 
