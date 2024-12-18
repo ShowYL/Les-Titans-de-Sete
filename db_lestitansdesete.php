5d1c19a37a10dc75f196d32699418662dbb1ff48
Yann L <130548077+ShowYL@users.noreply.github.com>
2024-12-18 17:32:44 +0100

    Refactor login logic to improve variable naming and remove commented-out code; streamline database query in db_lestitansdesete.php

--- db_lestitansdesete.php
+++ db_lestitansdesete.php
@@ -16,6 +16,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("La connexion a échoué: " . $conn->connect_error);
 }
-echo "Connexion réussie";
-
 ?>
--- login.php
+++ login.php
@@ -1,41 +1,20 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 session_start(); // Démarrer la session
-
-// $valid_email = 'user@example.com';
-// $valid_password = '123';
-
-// if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//     $email = $_POST['nomUtilisateur'];
-//     $password = $_POST['password'];
-
-//     if ($email === $valid_email && $password === $valid_password) {
-//         $_SESSION['loggedin'] = true;
-//         header('Location: acceuil.php');
-//         exit();
-//     } else {
-//         $error = 'Invalid email or password';
-//     }
-// }
-
 
 require 'db_lestitansdesete.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name = $_POST['nomUtilisateur'];
-    $password = $_POST['password'];
+    $passwordSend = $_POST['password'];
 
-    $stmt = $conn->prepare("SELECT * FROM User WHERE Nom_Utilisateur = ?");
+    $stmt = $conn->prepare("SELECT user FROM User WHERE Nom_Utilisateur = ?");
     $stmt->bind_param("s", $name);
     $stmt->execute();
     $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
         $user = $result->fetch_assoc();
-        if (password_verify($password, $user['password'])) {
+        if (password_verify($passwordSend, $user['password'])) {
             $_SESSION['loggedin'] = true;
             $_SESSION['user_id'] = $user['ID_User'];
             header('Location: acceuil.php');
