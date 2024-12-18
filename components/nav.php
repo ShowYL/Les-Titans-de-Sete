2c4eb68925c42c9b4ed11e10555110163d579ef1
Yann L <130548077+ShowYL@users.noreply.github.com>
2024-12-18 22:45:20 +0100

    Refactor navigation structure to use buttons with icons; update logo path and add new logo image

--- components/nav.php
+++ components/nav.php
@@ -1,13 +1,15 @@
 <!-- nav.php -->
 <div class='navi' >
     <nav>
-        <img src="../logo-white-version.png" alt="logo" class="logo">
+        <img src="images/logo-white-version.png" alt="logo" class="logo">
         <ul>
-            <li><a href="acceuil.php">Accueil</a></li>
-            <li><a href="joueurs.php">Joueurs</a></li>
-            <li><a href="matchs.php">Matchs</a></li>
-            <li><a href="statistique.php">Contact</a></li>
+            <li><button href="acceuil.php" class="nav-button"><img src="images\accueil.png" class="nav-image">Accueil</button></li>
+            <li><button href="joueurs.php" class="nav-button"><img src="images\joueur-de-rugby.png" class="nav-image">Joueurs</button></li>
+            <li><button href="matchs.php" class="nav-button"><img src="images\match.png" class="nav-image">Matchs</button></li>
+            <li><button href="statistique.php" class="nav-button">Contact</button></li>
         </ul>
-        <button>Log-Out</button>
+        <div class="logout-div">
+            <button class="logout-button"><img src="images\logout.png" class="nav-image">Log-Out</button>
+        </div>
     </nav>
 </div>
