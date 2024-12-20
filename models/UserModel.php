7f78e09449f4623910abf75d3e883d0e13770c10
Esther Paqui <paqui.esther@etu.iut-tlse3.fr>
2024-12-20 13:27:27 +0100

    Remove unused CSS and database connection files; update paths in models and views

--- models/UserModel.php
+++ models/UserModel.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../db_lestitansdesete.php';
+require_once 'db_lestitansdesete.php';
 
 class UserModel {
     private $conn;
--- views/match.php
+++ views/match.php
@@ -5,9 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Les Titants de Sete</title>
     <link rel="stylesheet" href="../style/table.css">
-    <link rel="stylesheet" href="../global-Style.css">
+    <link rel="stylesheet" href="../style/global-Style.css">
     <link rel="stylesheet" href="../style/headerfooter-style.css">
-    <link rel="stylesheet" href="../body_style.css">
 </head>
 <body>
     <div class="container">
