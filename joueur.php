e51b1d12713f76f35aa9beb1a11be39db2003ca1
Yann L <130548077+ShowYL@users.noreply.github.com>
2024-12-20 11:32:48 +0100

    Add table styling and createTable class for dynamic table generation

--- joueur.php
+++ joueur.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Les Titants de Sete</title>
+    <link rel="stylesheet" href="table.css">
     <link rel="stylesheet" href="global-Style.css">
     <link rel="stylesheet" href="headerfooter-style.css">
     <link rel="stylesheet" href="body_style.css">
